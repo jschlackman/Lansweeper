cdef600218359ff00985eb5e38109e2c0607ebba
jschlackman <jay@schlackman.org>
2018-06-18 18:43:51 -0400

    Disk: Physical Drive Details
    Lists model, serial, and firmware identifiers of the physical disk hardware present in assets.

---
+++ Reports/Disk_ Physical Drive Details.sql
@@ -0,0 +1,28 @@
+/* 
+   Disk: Physical Drive Details
+   
+   Lists model, serial, and firmware identifiers of the physical disk hardware
+   present in assets. This returns hard disk information despite it drawing
+   on the tblFloppy table.
+
+*/
+
+Select Top 1000000 tblAssets.AssetID,
+  tblAssets.AssetName,
+  tsysAssetTypes.AssetTypename,
+  tsysAssetTypes.AssetTypeIcon10 As icon,
+  tblAssets.IPAddress,
+  IsNull(tblADComputers.Description, tblAssets.Description) As Description,
+  tblFloppy.Name,
+  tblFloppy.Model,
+  tblFloppy.SerialNumber,
+  tblFloppy.FirmwareRevision,
+  tblAssets.Lastseen,
+  tblAssets.Lasttried
+From tblAssets
+  Inner Join tblAssetCustom On tblAssets.AssetID = tblAssetCustom.AssetID
+  Inner Join tsysAssetTypes On tsysAssetTypes.AssetType = tblAssets.Assettype
+  Left Join tblADComputers On tblAssets.AssetID = tblADComputers.AssetID
+  Inner Join tblFloppy On tblAssets.AssetID = tblFloppy.AssetID
+Where tblAssetCustom.State = 1
+Order By tblAssets.AssetName

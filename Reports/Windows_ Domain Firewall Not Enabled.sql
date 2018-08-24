a596bc1592c1eed1e1536726b1f3cb363e180745
jschlackman <jay@schlackman.org>
2018-08-24 18:01:29 -0400

    Now checks GPO policy as well as local policy

--- Reports/Windows_ Domain Firewall Not Enabled.sql
+++ Reports/Windows_ Domain Firewall Not Enabled.sql
@@ -2,19 +2,20 @@
    Windows: Domain Firewall Not Enabled
    
    Lists any Windows machine where Windows Firewall is not turned on for the Domain Profile.
-   Requires a custom registry scan configured as follows:
+   Requires custom registry scans configured as follows:
    
    Rootkey: HKEY_LOCAL_MACHINE
    RegPath: SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\DomainProfile
    RegValue: EnableFirewall
 
-*/
+   Rootkey: HKEY_LOCAL_MACHINE
+   RegPath: SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile
+   RegValue: EnableFirewall
    
+*/
+
 Select Top (1000000) tblAssets.AssetID,
   tblAssets.AssetName,
-  Case When tblRegistry.Value = 0 Then 'OFF'
-    When tblRegistry.Value = 1 Then 'ON' Else 'UNKNOWN'
-  End As DomainFirewallStatus,
   tsysAssetTypes.AssetTypename,
   tsysAssetTypes.AssetTypeIcon10 As icon,
   tblDomainroles.Domainrolename As DomainRole,
@@ -24,11 +25,19 @@ Select Top (1000000) tblAssets.AssetID,
 From tblAssets
   Inner Join tblAssetCustom On tblAssets.AssetID = tblAssetCustom.AssetID
   Inner Join tsysAssetTypes On tsysAssetTypes.AssetType = tblAssets.Assettype
-  Inner Join tblRegistry On tblAssets.AssetID = tblRegistry.AssetID
   Inner Join tblComputersystem On tblAssets.AssetID = tblComputersystem.AssetID
   Inner Join tblDomainroles On tblDomainroles.Domainrole =
     tblComputersystem.Domainrole
-Where Case When tblRegistry.Value = 0 Then 'OFF'
-    When tblRegistry.Value = 1 Then 'ON' Else 'UNKNOWN'
-  End <> 'ON' And tblAssetCustom.State = 1 And tblRegistry.Regkey =
-  N'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\DomainProfile' And tblRegistry.Valuename = N'EnableFirewall'
+  Left Join (Select *
+  From tblRegistry
+  Where
+    tblRegistry.Regkey =
+    N'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\DomainProfile' And tblRegistry.Valuename = N'EnableFirewall') As tblLocalFWPolicy On tblAssets.AssetID = tblLocalFWPolicy.AssetID
+  Left Join (Select *
+  From tblRegistry
+  Where
+    tblRegistry.Regkey =
+    N'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile' And tblRegistry.Valuename = N'EnableFirewall') As tblGPOFWPolicy On tblGPOFWPolicy.AssetID = tblAssets.AssetID
+Where IsNull(tblLocalFWPolicy.Value, 0) = 0 And IsNull(tblGPOFWPolicy.Value, 0) = 0 And tblAssetCustom.State = 1
+Order By DomainRole,
+  tblAssets.AssetName

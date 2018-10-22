8a90ae3e1fbede16f638eb9cd879a317dcffc389
jschlackman <jay@schlackman.org>
2018-10-21 22:39:23 -0400

    Windows: Template
    Enhanced template for Windows asset reports that includes domain role and description (from AD if filled in, otherwise from Lansweeper)

---
+++ Reports/Windows Template.sql
@@ -0,0 +1,26 @@
+/*
+	Windows: Template
+	
+	Enhanced template for Windows asset reports that includes domain role and
+	description (from AD if filled in, otherwise from Lansweeper)
+	
+*/
+
+Select Top 1000000 tblAssets.AssetID,
+  tblAssets.AssetName,
+  tsysAssetTypes.AssetTypeIcon10 As icon,
+  tblDomainroles.Domainrolename As DomainRole,
+  IsNull(tblADComputers.Description, tblAssets.Description) As Description,
+  tblAssets.IPAddress,
+  tblAssets.Lastseen,
+  tblAssets.Lasttried
+From tblAssets
+  Inner Join tblAssetCustom On tblAssets.AssetID = tblAssetCustom.AssetID
+  Inner Join tsysAssetTypes On tsysAssetTypes.AssetType = tblAssets.Assettype
+  Left Join tblADComputers On tblAssets.AssetID = tblADComputers.AssetID
+  Inner Join tblComputersystem On tblAssets.AssetID = tblComputersystem.AssetID
+  Inner Join tblDomainroles On tblDomainroles.Domainrole =
+    tblComputersystem.Domainrole
+Where tblAssetCustom.State = 1
+Order By tblDomainroles.Domainrole Desc,
+  tblAssets.AssetName

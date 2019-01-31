0a840cea0c437a390c1b4591bc7c696a328eb990
jschlackman <jay@schlackman.org>
2019-01-30 20:26:59 -0500

    Battery: Detail
    Detail from the Win32_PortableBattery class. More accurate and useful than the built-in "Battery: Information" report.

---
+++ Reports/Battery_ Detail.sql
@@ -0,0 +1,27 @@
+/*
+	Battery: Detail
+	
+	Detail from the Win32_PortableBattery class. More accurate and useful than
+	the built-in "Battery: Information" report.
+
+*/
+
+Select Top 1000000 tblAssets.AssetID,
+  tblAssets.AssetName,
+  tsysAssetTypes.AssetTypeIcon10 As icon,
+  IsNull(tblADComputers.Description, tblAssets.Description) As Description,
+  tblPortableBattery.Manufacturer,
+  tblPortableBattery.Name,
+  tblPortableBattery.ManufactureDate,
+  Format(Round(tblPortableBattery.DesignCapacity / 1000, 0), '# Wh') As
+  DesignCapacity,
+  Format(Round(tblPortableBattery.DesignVoltage / 1000, 1), '#.# V') As
+  DesignVoltage
+From tblAssets
+  Inner Join tblAssetCustom On tblAssets.AssetID = tblAssetCustom.AssetID
+  Inner Join tsysAssetTypes On tsysAssetTypes.AssetType = tblAssets.Assettype
+  Left Join tblADComputers On tblAssets.AssetID = tblADComputers.AssetID
+  Inner Join tblPortableBattery On
+    tblAssets.AssetID = tblPortableBattery.AssetID
+Where tblAssetCustom.State = 1
+Order By tblAssets.AssetName

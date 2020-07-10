978d2edfbff2135dbf419b89dc5ca01e633df495
J. Schlackman <james@schlackman.org>
2020-07-10 12:57:58 -0400

    Enhanced version of last seen reports
    Enhanced version of last seen reports that include domain role and AD description for Windows computers, and also show Mac computers.

---
+++ Reports/Computer_ Not seen in the last 90 days.sql
@@ -0,0 +1,29 @@
+/*
+   Computer: Not seen in the last 90 days
+   
+   Enhanced version of last seen report that includes domain role and AD
+   description for Windows computers, and also shows Mac computers.
+
+*/
+
+Select Top (1000000) tblAssets.AssetID,
+  tblAssets.AssetName,
+  tsysAssetTypes.AssetTypeIcon10 As icon,
+  tblDomainroles.Domainrolename As DomainRole,
+  IsNull(tblADComputers.Description, tblAssets.Description) As Description,
+  tblAssets.IPAddress,
+  tblAssets.Lastseen As [Last Seen],
+  tblAssets.Lasttried As [Last Tried]
+From tblDomainroles
+  Inner Join tblComputersystem On tblDomainroles.Domainrole =
+    tblComputersystem.Domainrole
+  Right Outer Join (tblAssets
+  Inner Join tblAssetCustom On tblAssets.AssetID = tblAssetCustom.AssetID
+  Inner Join tsysAssetTypes On tsysAssetTypes.AssetType = tblAssets.Assettype
+  Left Outer Join tblADComputers On tblAssets.AssetID = tblADComputers.AssetID)
+    On tblComputersystem.AssetID = tblAssets.AssetID
+Where tblAssets.Lastseen < GetDate() - 90 And tblAssetCustom.State = 1
+  And (tblAssets.Assettype = -1 Or tblAssets.Assettype = 13)
+Order By [Last Seen] Desc,
+  tblDomainroles.Domainrole Desc,
+  tblAssets.AssetName

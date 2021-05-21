fbd3883d25f0f38950a8c74bbc66fbe36c6e24bf
J. Schlackman <james@schlackman.org>
2021-05-21 10:32:08 -0400

    Update Asset_ All Assigned Workstations.sql
    Add details of the first known software history scan to help determine first usage date of equipment vs. first seen (which may be when unused equipment was entered into inventory as new Stock)

--- Reports/Asset_ All Assigned Workstations.sql
+++ Reports/Asset_ All Assigned Workstations.sql
@@ -13,7 +13,8 @@ Select Top (1000000) tblAssets.AssetID,
   tblOperatingsystem.Caption As OSName,
   Cast(tblAssetCustom.Warrantydate As Date) As WarrantyDate,
   tblAssets.Firstseen,
-  tblAssets.Lastseen  
+  tblAssets.Lastseen,
+  SoftwareHist.FirstScan
 From tblAssets
   Inner Join tblAssetCustom On tblAssets.AssetID = tblAssetCustom.AssetID
   Left Outer Join tsysAssetTypes On tsysAssetTypes.AssetType =
@@ -24,5 +25,10 @@ From tblAssets
   Left Join tblADComputers On tblAssets.AssetID = tblADComputers.AssetID
   Inner Join tblOperatingsystem On
     tblAssets.AssetID = tblOperatingsystem.AssetID
+  Inner Join (Select Min(tblSoftwareHist.Lastchanged) As FirstScan,
+        tblSoftwareHist.AssetID
+      From tblSoftwareHist
+      Group By tblSoftwareHist.AssetID) SoftwareHist On SoftwareHist.AssetID =
+    tblAssets.AssetID
 Where tblADComputers.ManagerADObjectId Is Not Null And
   tblDomainroles.Domainrole = 1 And tblAssetCustom.State = 1

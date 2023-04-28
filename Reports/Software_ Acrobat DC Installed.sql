120a0dd5095e4f4fb02f3dab6cfc0d5a0771614d
James Schlackman <jschlackman@CAQH.ORG>
2023-04-28 15:10:39 -0400

    Update Software_ Acrobat DC Installed.sql

--- Reports/Software_ Acrobat DC Installed.sql
+++ Reports/Software_ Acrobat DC Installed.sql
@@ -1,13 +1,14 @@
 /* 
-	Software: Acrobat DC Installed
+	Software: Adobe Acrobat Installed
 	
-	Report of workstations with some version of Acrobat DC installed.
+	Report of workstations with some version of Acrobat (not Reader) installed.
 	
 */
 
 Select Top 1000000 tblAssets.AssetID,
   tblAssets.AssetName,
   tsysAssetTypes.AssetTypeIcon10 As icon,
+  AcrobatInstalls.softwareVersion,
   IsNull(tblADComputers.Description, tblAssets.Description) As Description,
   tblAssets.Username,
   tblAssets.IPAddress,
@@ -20,10 +21,13 @@ From tblAssets
   Inner Join tblComputersystem On tblAssets.AssetID = tblComputersystem.AssetID
   Inner Join tblDomainroles On tblDomainroles.Domainrole =
       tblComputersystem.Domainrole
-Where tblAssets.AssetID In (Select Top 1000000 tblSoftware.AssetID
-    From tblSoftware Inner Join tblSoftwareUni On tblSoftwareUni.SoftID =
-          tblSoftware.softID
-    Where tblSoftwareUni.softwareName Like '%Acrobat DC%') And
-  tblAssetCustom.State = 1
+  Inner Join (Select tblSoftware.AssetID,
+      tblSoftware.softwareVersion
+    From tblSoftware
+      Inner Join tblSoftwareUni On tblSoftwareUni.SoftID = tblSoftware.softID
+    Where tblSoftwareUni.softwareName Like '%Acrobat%' And
+      tblSoftwareUni.softwareName Not Like '%Reader%' And tblSoftware.MsStore =
+      0) AcrobatInstalls On AcrobatInstalls.AssetID = tblAssets.AssetID
+Where tblAssetCustom.State = 1
 Order By tblDomainroles.Domainrole Desc,
   tblAssets.AssetName

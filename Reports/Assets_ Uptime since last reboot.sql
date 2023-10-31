cc96b054f1035d990d61eafb19056fd4be1ff90b
James Schlackman <jschlackman@CAQH.ORG>
2023-10-31 12:04:40 -0400

    Update Assets_ Uptime since last reboot.sql
    Uses live uptime calculations for Windows assets instead of returning the uptime at last scan

--- Reports/Assets_ Uptime since last reboot.sql
+++ Reports/Assets_ Uptime since last reboot.sql
@@ -1,21 +1,16 @@
 /*
 	Assets: Uptime since last reboot
 	
-	Improved version of report that replaces the obsolete service pack field with the OS version/release identifier.
+	Improved version of report that replaces the obsolete service pack field with the OS version/release identifier
+	and uses live uptime calculations for Windows assets instead of returning the uptime at last scan
 
 */
 Select Top 1000000 tblAssets.AssetID,
   tblAssets.AssetName,
-  tblAssets.Domain,
   tsysAssetTypes.AssetTypename As AssetType,
-  Convert(nvarchar(10),Ceiling(Floor(Convert(integer,tblAssets.Uptime) / 3600 /
-  24))) + ' days ' +
-  Convert(nvarchar(10),Ceiling(Floor(Convert(integer,tblAssets.Uptime) / 3600 %
-  24))) + ' hours ' +
-  Convert(nvarchar(10),Ceiling(Floor(Convert(integer,tblAssets.Uptime) % 3600 /
-  60))) + ' minutes' As UptimeSinceLastReboot,
-  tblAssets.Lastseen - Convert(Decimal,tblAssets.Uptime / 60 / 60 /
-  24) As [Boot Time],
+  Convert(nvarchar(10),Ceiling(Floor(Convert(integer,IsNull(tblBootTime.UptimeSeconds, tblAssets.Uptime)) / 3600 / 24))) + ' days ' + Convert(nvarchar(10),Ceiling(Floor(Convert(integer,IsNull(tblBootTime.UptimeSeconds, tblAssets.Uptime)) / 3600 % 24))) + ' hours ' + Convert(nvarchar(10),Ceiling(Floor(Convert(integer,IsNull(tblBootTime.UptimeSeconds, tblAssets.Uptime)) % 3600 / 60))) + ' minutes' As UptimeSinceLastReboot,
+  IsNull(tblBootTime.BootTime, tblAssets.Lastseen -
+  Convert(float,tblAssets.Uptime / 60 / 60 / 24)) As [Boot Time],
   tblAssets.Username,
   tblAssets.Userdomain,
   tsysAssetTypes.AssetTypeIcon10 As icon,
@@ -37,8 +32,14 @@ From tblAssets
   Inner Join tblState On tblState.State = tblAssetCustom.State
   Left Join tblLinuxSystem On tblAssets.AssetID = tblLinuxSystem.AssetID
   Left Join tblMacOSInfo On tblAssets.AssetID = tblMacOSInfo.AssetID
-Where tblState.Statename = 'Active' And tblAssets.Uptime Is Not Null
-Order By tblAssets.Uptime Desc,
+  Left Join (Select Top 1000000 tblUptime.AssetID,
+      Max(tblUptime.EventTime) As BootTime,
+      Min(DateDiff(second, tblUptime.EventTime, GetDate())) As UptimeSeconds
+    From tblUptime
+    Group By tblUptime.AssetID) tblBootTime On tblBootTime.AssetId =
+      tblAssets.AssetID
+Where tblAssets.Uptime Is Not Null And tblState.Statename = 'Active'
+Order By IsNull(tblBootTime.UptimeSeconds, tblAssets.Uptime) Desc,
   tblAssets.IPNumeric,
   tblAssets.Domain,
   tblAssets.AssetName

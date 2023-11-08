89e3159031b68f420df37185f205a31b55c147b3
James Schlackman <jschlackman@CAQH.ORG>
2023-11-08 12:28:21 -0500

    Windows: All local user accounts
    Lists all local user accounts on active Windows systems.

---
+++ Reports/Windows_ All local user accounts.sql
@@ -0,0 +1,32 @@
+/*
+	Windows: All local user accounts
+	
+	Lists all local user accounts on active Windows systems.
+	
+*/
+Select Top 1000000 tblAssets.AssetID,
+  tblAssets.AssetName,
+  tsysAssetTypes.AssetTypeIcon10 As icon,
+  tblDomainroles.Domainrolename As DomainRole,
+  IsNull(tblADComputers.Description, tblAssets.Description) As Description,
+  tblAssets.IPAddress,
+  tblUsers.Name,
+  tblUsers.Fullname,
+  tblUsers.BuildInAdmin As BuiltInAdmin,
+  tblUsers.Disabled,
+  tblUsers.PasswordChangeable,
+  tblUsers.PasswordRequired,
+  tblUsers.Lastchanged,
+  tblAssets.Lastseen,
+  tblAssets.Lasttried
+From tblAssets
+  Inner Join tblAssetCustom On tblAssets.AssetID = tblAssetCustom.AssetID
+  Inner Join tsysAssetTypes On tsysAssetTypes.AssetType = tblAssets.Assettype
+  Left Join tblADComputers On tblAssets.AssetID = tblADComputers.AssetID
+  Inner Join tblComputersystem On tblAssets.AssetID = tblComputersystem.AssetID
+  Inner Join tblDomainroles On tblDomainroles.Domainrole =
+      tblComputersystem.Domainrole
+  Inner Join tblUsers On tblAssets.AssetID = tblUsers.AssetID
+Where tblAssetCustom.State = 1
+Order By tblDomainroles.Domainrole Desc,
+  tblAssets.AssetName

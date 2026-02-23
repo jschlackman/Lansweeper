9e63a959fff690db4511c47bd1d246ef9a929bfb
James Schlackman <james@schlackman.org>
2026-02-23 15:39:26 -0500

    Update Windows Active Directory Writeback.sql
    - Reference AD computers by GUID instead of name
    - Get assigned user information from a user asset relation if one exists

--- Reports/Windows Active Directory Writeback.sql
+++ Reports/Windows Active Directory Writeback.sql
@@ -4,23 +4,26 @@
    Lists Windows computer details to be written back to Active Directory using the Lansweeper-UpdateComputers.ps1 script.
 
 */
-SELECT
-    dbo.tblADObjects.sAMAccountName,
-    dbo.tblADComputers.OU,
-    dbo.tblAssetCustom.Manufacturer,
-    dbo.tblAssetCustom.Model,
-    dbo.tblAssetCustom.SystemSKU,
-    dbo.tblAssetCustom.Contact,
-    dbo.tblAssetCustom.Serialnumber,
-    dbo.tblAssetCustom.Location
-FROM
-    dbo.tblComputersystem INNER JOIN
-    dbo.tblADComputers ON dbo.tblComputersystem.AssetID = dbo.tblADComputers.AssetID INNER JOIN
-    dbo.tblADObjects ON dbo.tblADComputers.ADObjectID = dbo.tblADObjects.ADObjectID INNER JOIN
-    dbo.tblAssetCustom ON dbo.tblADComputers.AssetID = dbo.tblAssetCustom.AssetID
-WHERE
-    (NOT (dbo.tblAssetCustom.Model IS NULL)) AND
-    (dbo.tblAssetCustom.State = 1) AND
-    (dbo.tblComputersystem.Domainrole = 1)
-ORDER BY
-    dbo.tblADObjects.sAMAccountName
+Select
+  tblADComputers.ObjectGUID,
+  tblAssetCustom.Manufacturer,
+  tblAssetCustom.Model,
+  tblAssetCustom.SystemSKU,
+  tblAssetCustom.Contact,
+  tblAssetCustom.Serialnumber,
+  tblAssetCustom.Location,
+  AssignedUsers.UserDisplayName
+From tblComputersystem
+  Inner Join tblADComputers On tblComputersystem.AssetID =
+      tblADComputers.AssetID
+  Inner Join tblAssetCustom On tblADComputers.AssetID = tblAssetCustom.AssetID
+  Left Join (Select tblAssetUserRelations.AssetID,
+      (tblADusers.Firstname + ' ' + tblADusers.Lastname) As UserDisplayName
+    From tblAssetUserRelations
+      Inner Join tblADusers On
+          tblADusers.Username = tblAssetUserRelations.Username And
+          tblADusers.Userdomain = tblAssetUserRelations.Userdomain
+    Where tblAssetUserRelations.EndDate Is Null And tblAssetUserRelations.Type =
+      200) AssignedUsers On AssignedUsers.AssetID = tblComputersystem.AssetID
+Where Not (tblAssetCustom.Model Is Null) And tblAssetCustom.State = 1 And
+  tblComputersystem.Domainrole = 1
